2cb714d4ba237ffa2a375cca4a1f5c0dde232ea7
Christian Oleson <christian.oleson@imaginelearning.com>
2022-03-13 23:18:41 -0500

    Chapter 1 - Exercise 1.1

---
+++ Chapter 1/Exercises/1.1/src/com/oleson/Main.java
@@ -0,0 +1,34 @@
+package com.oleson;
+
+public class Main {
+    private static final int characterCountPerLine = 22;
+
+    public static void main(String[] args) {
+        WriteCharacterByQuantity(characterCountPerLine, '/');
+        WriteMessageWithinLineLength("Victory is mine!", characterCountPerLine);
+        WriteCharacterByQuantity(characterCountPerLine, '\\');
+    }
+
+    private static void WriteMessageWithinLineLength(String message, int lineLength) {
+        String sideBar = "||";
+        int messageLength = message.length();
+        int sideBarLength = sideBar.length();
+        int countEmptySpacePerSideSize = (lineLength - (messageLength + (sideBarLength * 2))) / 2;
+        String emptySpacePerSide = "";
+
+        for (int i = 0; i < countEmptySpacePerSideSize; i++) {
+            emptySpacePerSide += " ";
+        }
+
+        String finalMessage = sideBar + emptySpacePerSide + message + emptySpacePerSide + sideBar;
+        System.out.println(finalMessage);
+    }
+
+    private static void WriteCharacterByQuantity(int count, char character){
+        var sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(character);
+        }
+        System.out.println(sb);
+    }
+}
