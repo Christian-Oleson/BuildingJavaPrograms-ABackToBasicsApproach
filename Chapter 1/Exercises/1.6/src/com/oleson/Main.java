4b483825dd7f5d1cd339c5e4291053562811519e
Christian Oleson <christian.oleson@imaginelearning.com>
2022-03-15 22:28:00 -0500

    Ch 1 - Exercise 1.6

---
+++ Chapter 1/Exercises/1.6/src/com/oleson/Main.java
@@ -0,0 +1,11 @@
+package com.oleson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(MetaString);
+    }
+
+    private static String MetaString = "package com.oleson;\n\npublic class Main {\n    public static void main(" +
+            "String[] args) {\n        System.out.println(\"Hello, world!\");\n   }\n}";
+}
