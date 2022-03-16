d260c18a29cf0e261a45ad0fb4c8b828baf875a5
Christian Oleson <christian.oleson@imaginelearning.com>
2022-03-15 22:46:33 -0500

    Ch 1 - Exercise 1.9

---
+++ Chapter 1/Exercises/1.9/src/com/oleson/Main.java
@@ -0,0 +1,15 @@
+package com.oleson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println("  _______ ");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\       /");
+        System.out.println("  ﹊﹊﹊﹊ ");
+    }
+}
